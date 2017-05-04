325606b1e4d0d1309a51b16376ac6b860dafd9fc
Justin Haas <justinmhaas1@gmail.com>
2017-05-03 20:10:25 -0400

    Finished Superhuman

--- src/main/java/com/haasjustin/com/haasjustin/superhero/Human.java
+++ src/main/java/com/haasjustin/com/haasjustin/superhero/Human.java
@@ -3,10 +3,65 @@ package com.haasjustin.com.haasjustin.superhero;
 /**
  * Created by justinhaas on 5/3/17.
  */
-public class Human {
 
 
+class Human {
 
+    private String name;
+    private int age;
+    private String gender;
+    private String occupation;
+    private String address;
 
+    public Human() {
+    }
+
+    public Human(String name, int age, String gender, String occupation, String address) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.occupation = occupation;
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public String getOccupation() {
+        return this.occupation;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
 
 }
--- src/main/java/com/haasjustin/com/haasjustin/superhero/Superhuman.java
+++ src/main/java/com/haasjustin/com/haasjustin/superhero/Superhuman.java
@@ -1,8 +1,64 @@
 package com.haasjustin.com.haasjustin.superhero;
 
+import java.util.Scanner;
+
 /**
  * Created by justinhaas on 5/3/17.
  */
-public class Superhuman {
+public class Superhuman extends Human {
 
+    private String alliance;
+    private String power;
+
+    public Superhuman(String superName, String theirAlliance, String theirPower) {
+        super();
+        setName(superName);
+        alliance = theirAlliance;
+        power = theirPower;
+    }
+
+    public void setAlliance(String theirAlliance) {
+        alliance = theirAlliance;
+    }
+
+    public void setPower(String theirPower) {
+        power = theirPower;
+    }
+
+    public String getAlliance() {
+        return alliance;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        Superhuman batman = new Superhuman("Batman", "Justice League", "Literally the best");
+        batman.setAge(30);
+        batman.setAddress("Gotham City");
+        batman.setOccupation("Millionaire");
+        batman.setGender("Male");
+        batman.setAlliance("Justice League");
+        batman.setPower("Literally the best");
+        Scanner scn = new Scanner(System.in);
+        System.out.println(scn.next("Name: " + " " + batman.getName()));
+
+        System.out.println(scn.next("Age: " + " " + batman.getAge()));
+
+        System.out.println(scn.next("Gender: " + " " + batman.getGender()));
+
+        System.out.println(scn.next("Address: " + " " + batman.getAddress()));
+
+        System.out.println(scn.next("Occupation: " + " " + batman.getOccupation()));
+
+        System.out.println(scn.next("Alliance: " + " " + batman.getAlliance()));
+
+        System.out.println(scn.next("Power: " + " " + batman.getPower()));
+        scn.close();
+    }
 }
+
